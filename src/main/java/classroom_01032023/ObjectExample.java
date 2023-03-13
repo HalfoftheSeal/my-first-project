c03ffad59d2577a8a821981823cd516cfaa6e463
HalfoftheSeal <maksim0shilov@gmail.com>
2023-03-13 21:06:23 +0200

    [classroom_13032023]: Adds classroom progress

---
+++ src/main/java/classroom_01032023/ObjectExample.java
@@ -0,0 +1,23 @@
+package classroom_01032023;
+
+public class ObjectExample {
+    public static void main(String[] args) {
+        Cat barsik = new Cat("Barsik");
+        System.out.println(barsik.getAge());
+        System.out.println(barsik);
+        barsik.setAge(4);
+        System.out.println(barsik.getAge());
+        barsik.setWeight(5.4);
+        System.out.println(barsik.getWeight());
+
+        barsik.speak();
+        barsik.feed();
+
+        Cat mark = new Cat("Mark", "brown", 4, 5.4, "Persian", "male", true);
+        System.out.printf("My cat name is: " + mark.getName() + ". He is " + mark.getColor() + " and " + mark.getAge() + " years old. He weights " + mark.getWeight() + " kg. He is a " + mark.getBreed() + " cat. He is a " + mark.getGender() + ".\n");
+        mark.walk();
+        System.out.println(mark.getEnergy());
+        mark.feed();
+        System.out.println(mark.getEnergy());
+    }
+}
